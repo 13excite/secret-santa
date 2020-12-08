b9f0f40c9f4777fb08d76674f1ceaf8e9aa02dc7
Dulenov Vladimir <v.dulenov@corp.vk.com>
2020-12-08 15:31:13 +0300

    Add simple view

--- santa/base/views.py
+++ santa/base/views.py
@@ -6,5 +6,5 @@ base_blueprint = Blueprint('base', __name__, template_folder='templates')
 
 @base_blueprint.route('/')
 def get_main_page():
-    return jsonify({'status': 'OK'})
+    return render_template('general.html')
 
