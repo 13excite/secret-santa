b09660ce161cb11ab9a5931532b3b25d920296b5
Dulenov Vladimir <v.dulenov@corp.vk.com>
2020-12-09 23:31:45 +0300

    Add base model

---
+++ santa/base/models.py
@@ -0,0 +1,19 @@
+from santa.db import db
+
+
+class Member(db.Model):
+    __tablename__ = 'members'
+
+    member_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(30))
+    interest = db.Column(db.String(254))
+    email = db.Column(db.String(150))
+
+    def __init__(self, member_id, name, interest, email):
+        self.name = name
+        self.member_id = member_id
+        self.interest = interest
+        self.email = email
+
+    def __repr__(self):
+        return f'<Category {self.member_id}>'
