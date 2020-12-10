e83764fa1f8644351f3ee5f2aebc82209bea20c8
Dulenov Vladimir <v.dulenov@corp.vk.com>
2020-12-10 11:52:41 +0300

    Add dao methods

---
+++ santa/base/dao.py
@@ -0,0 +1,15 @@
+from santa.db import db
+from santa.base.models import Member
+
+
+def insert_member_to_db(member_dict):
+    try:
+        db.session.add(Member(name=member_dict['name'], interest=member_dict['interest'], email=member_dict['email']))
+        db.session.commit()
+        return 1
+    except KeyError:
+        return 0
+
+
+def check_uniq_email(email):
+    return Member.query.with_entities(Member.email).filter(Member.email == email).first()
